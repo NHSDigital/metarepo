3fc5e453384e6ec1a3aba8306d50727cdb0b1702
Laurence Pakenham-Smith <laurence.smith@airelogic.com>
2021-11-30 12:48:13 +0000

    APM-0000 Fix set-ticket-links

--- set-ticket-links.sh
+++ set-ticket-links.sh
@@ -2,15 +2,27 @@
 
 declare -A PROJECTS
 
-NHSD_JIRA_URL="https://nhsd-jira.digital.nhs.uk/browse/<num>"
+NHSD_JIRA_URL="https://nhsd-jira.digital.nhs.uk/browse"
 PROJECTS=( \
-    ["APM"]="$NHSD_JIRA_URL" \
-    ["AMB"]="$NHSD_JIRA_URL" \
-    ["APMSPII"]="$NHSD_JIRA_URL" \
+    ["APM"]="$NHSD_JIRA_URL/APM-<num>" \
+    ["AMB"]="$NHSD_JIRA_URL/AMB-<num>" \
+    ["APMSPII"]="$NHSD_JIRA_URL/APMSPII-<num>" \
 )
 
+AUTOLINKS=$(curl "https://api.github.com/repos/NHSDigital/$REPO_NAME/autolinks" \
+                 -H "Authorization: Bearer $GITHUB_PAT")
+AUTOLINKS=$(echo $AUTOLINKS | jq -r '.[] | (.id|tostring) + " " + .key_prefix')
 
 for PROJECT in "${!PROJECTS[@]}"; do
+    ID=$(echo "$AUTOLINKS" | grep $PROJECT- | cut -d' ' -f1)
+    if [[ -n $ID ]]; then
+        echo "https://api.github.com/repos/NHSDigital/$REPO_NAME/autolinks/$ID"
+        curl "https://api.github.com/repos/NHSDigital/$REPO_NAME/autolinks/$ID" \
+            -X DELETE \
+            -H "Accept: application/vnd.github.v3+json" \
+            -H "Authorization: Bearer $GITHUB_PAT"
+    fi
+
     curl "https://api.github.com/repos/NHSDigital/$REPO_NAME/autolinks" \
         -X POST \
         -H "Accept: application/vnd.github.v3+json" \
